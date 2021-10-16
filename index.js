84d9a53daf1ce65c9eb0cfb77b00902b3b0847d5
Maurice Smith <Xmaurice.smith@gmail.com>
2021-10-15 22:13:32 -0400

    commit tree d05505a2d81216f0b9be2d7e6e56a85550613b95

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let totalBatteries = batteryBatches.reduce(function(accumulator, batteries){ return batteries + accumulator},0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const reducer = (accumulator, item) => {
+//     console.log(accumulator, item)
+//     let total = item.price * item.iventory 
+//     return accumulator+= total 
+
+// }
+
+// let total = books.reducer(reducer,0)
+
+//.reducer can do more than just math 
